3158b58f3bda70fce3ee7d073cdb991b738b33d4
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-02 01:08:02 +0530

    completed the data ingestion in modular coding

---
+++ main.py
@@ -0,0 +1,28 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.logging import logger
+
+
+
+def run_pipeline(stage_name, pipeline_instance):
+    """
+    Run a specific stage of the sentiment analysis pipeline.
+
+    Parameters:
+    - stage_name: str
+        Name of the pipeline stage.
+    - pipeline_instance: object
+        Instance of the pipeline stage to be executed.
+
+    Returns:
+        None
+    """
+    try:
+        logger.info(f">>>>>> Stage {stage_name} started <<<<<<")
+        pipeline_instance.main()
+        logger.info(f">>>>>> Stage {stage_name} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+if __name__ == "__main__":
+    run_pipeline("Data Ingestion", DataIngestionTrainingPipeline())
--- template.py
+++ template.py
@@ -18,6 +18,7 @@ list_of_files = [
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
