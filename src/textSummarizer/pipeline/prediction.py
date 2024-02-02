6fd0224643b0441f16e9d148ad2c234e909f216c
Izam Mohammed <izamdeveloper1@gmail.com>
2024-02-02 17:10:20 +0530

    added the prediction

---
+++ src/textSummarizer/pipeline/prediction.py
@@ -0,0 +1,24 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.logging import logger
+from transformers import AutoTokenizer
+from transformers import pipeline
+
+
+class PredictionPipeline:
+    def __init__(self):
+        self.config = ConfigurationManager().get_model_evaluation_config()
+    
+    def predict(self,text):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+
+        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+
+        logger.info("Dialogue:")
+        logger.info(text)
+
+        output = pipe(text, **gen_kwargs)[0]["summary_text"]
+        logger.info("\nModel Summary:")
+        logger.info(output)
+
+        return output
